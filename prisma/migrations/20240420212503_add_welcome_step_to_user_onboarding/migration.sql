f7c4d8e7510d4011aaca58acc82659883329ec67
Daniel Wilder <dauncy@gmail.com>
2024-05-10 18:37:01 -1000

    squash migrations

--- prisma/migrations/20240420212503_add_welcome_step_to_user_onboarding/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterEnum
-ALTER TYPE "UserOnboardingStep" ADD VALUE 'WELCOME';
--- prisma/migrations/20240427062520_add_user_role/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "UserRole" AS ENUM ('authenticated', 'admin');
-
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "roles" "UserRole"[] DEFAULT ARRAY[]::"UserRole"[];
